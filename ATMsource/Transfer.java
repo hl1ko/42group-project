0f2e8e0fb4940dc11c3ceb8f1e5628b1a92e6c8f
PhysicalNoob_ <PhysicalNoob@gmail.com>
2025-10-15 15:35:50 +0800

    double confirm

--- ATMsource/Transfer.java
+++ ATMsource/Transfer.java
@@ -1,5 +1,3 @@
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 public class Transfer extends Transaction{
     private double amount; // amount to transfer
@@ -41,21 +39,33 @@ public class Transfer extends Transaction{
                 screen.displayMessageLine("Target Account does not exist.");
                 screen.displayMessage("Please enter again: ");
             }
-            
         } while ((targetAccountNumber == getAccountNumber() || bankDatabase.checkAccountExist(targetAccountNumber) == false) && targetAccountNumber != 0);
         
-        amount = promptForTransferAmount();
-
-        if(bankDatabase.getAvailableBalance(getAccountNumber()) < amount){
-         screen.displayMessageLine("You do not have sufficient balance.");
-        }else if(amount == 0){
+         amount = promptForTransferAmount();
+         if(amount == CANCELED){
          screen.displayMessageLine("Transfer cancelled.");
-        }else{
+         return;
+         }
+         if(bankDatabase.getAvailableBalance(getAccountNumber()) < amount){
+         screen.displayMessageLine("You do not have sufficient balance.");
+         return;
+         }
+
+         screen.displayMessageLine("=====");
+         screen.displayMessageLine("Recipient ID: " + targetAccountNumber);
+         screen.displayMessageLine("Transfer amount: " + amount);
+         screen.displayMessageLine("=====");
+         screen.displayMessageLine("Please enter 1 if and only if you did confirm that the transfer info above is correct and wish to proceed the following transfer action");
+         screen.displayMessage("Confirm?: ");
+         if(keypad.getInput() != 1){
+            screen.displayMessageLine("Transfer Action canceled");
+            return;
+         }
+
          bankDatabase.debit(getAccountNumber(), amount);
          bankDatabase.credit(targetAccountNumber, amount);
          screen.displayMessageLine("Transfer success.");
          screen.displayMessageLine("NOTE: The money just transfer will not be available until we verify the transacation.");
-        }
     }
 
    private double promptForTransferAmount()
@@ -66,7 +76,12 @@ public class Transfer extends Transaction{
       screen.displayMessage( "\nPlease enter a transfer amount in " + 
          "Dollars (or 0 to cancel) up to maximun of two digits (.00): " );
       float input = keypad.getInputFloat(); // receive input of deposit amount
-      go2digits(input);
+      screen.displayMessage( "\nPlease enter the transfer amount again: " );
+      float input1 = keypad.getInputFloat(); // receive input of deposit amount
+      if(input1 != input){
+         screen.displayMessageLine( "\nThe transfer amount did not match. Cancelling transfer action." );
+         return CANCELED;
+      }
       // check whether the user canceled or entered a valid amount
       if ( input == CANCELED ) 
          return CANCELED;
@@ -74,8 +89,5 @@ public class Transfer extends Transaction{
       {
          return ( double ) input; // return dollar amount 
       } // end else
-   }
-   public static double go2digits(double input){
-   return BigDecimal.valueOf(input).setScale(2, RoundingMode.FLOOR).doubleValue();
    }
 }
